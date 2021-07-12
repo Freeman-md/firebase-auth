996f559526bc1dd5af350443e43a750374e4bae7
Freeman-md <freemanmadudili@gmail.com>
2021-07-12 10:06:48 +0100

    Adding guides

--- scripts/auth.js
+++ scripts/auth.js
@@ -15,6 +15,27 @@ auth
     }
   })
 
+// create new guide
+const createForm = document.querySelector('#create-form')
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  db
+    .collection('guides')
+    .add({
+      title: createForm['title'].value,
+      content: createForm['content'].value
+    })
+    .then(() => {
+      const modal = document.querySelector('#modal-create')
+      M.Modal.getInstance(modal).close()
+      createForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
 // signup
 const signUpForm = document.querySelector('#signup-form')
 signUpForm.addEventListener('submit', (e) => {
